cab9f2dcdbb1035ff9fdbc9e341d7c41029baf65
Furlinastis <dunkelnoemi@gmail.com>
2020-07-27 12:39:15 +0200

    Galaxis utikalaut stopposoknak

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World meg 42 es minden ami kell");
+        System.out.println("There is a theory which states that if ever anyone discovers exactly what the Universe is for and why it is here");
+        String szoveg;
+        szoveg = "it will instantly disappear and be replaced by something even more bizarre and inexplicable.";
+        System.out.println(szoveg);
+        String szoveg2;
+        szoveg2= "There is another theory which states that this has already happened.";
+        System.out.println(szoveg2);
     }
 }
